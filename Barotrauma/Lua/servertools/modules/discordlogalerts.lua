c0c46ede1e1759bac16719e4b25dacc2a15d3404
EvilFactory <evilfactory@users.noreply.github.com>
2022-10-31 15:37:49 -0300

    Fix crash

--- Barotrauma/Lua/servertools/modules/discordlogalerts.lua
+++ Barotrauma/Lua/servertools/modules/discordlogalerts.lua
@@ -128,7 +128,7 @@ module.OnEnabled = function ()
                     message = message:gsub("‖metadata:", "`")
                     message = message:gsub("‖end‖", "***")
                     message = message:gsub("‖", "`***")
-                    Timer.Wait(SendMessage(message), 1000)
+                    Timer.Wait(function() SendMessage(message) end, 1000)
                     break
                 end
             end
